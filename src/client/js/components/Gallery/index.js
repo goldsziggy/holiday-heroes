0516a167581ae188f65ebc010e8ee771986f6889
Matt Zygowicz <matt@zygowicz.com>
2017-01-22 16:54:26 -0600

    Minor fixes to the gallery

--- src/client/js/components/Gallery/index.js
+++ src/client/js/components/Gallery/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import * as sliderActions from '../../actions/sliderActions';
-import Slider from 'react-slick';
+// import Slider from 'react-slick';
+import ImageGallery from 'react-image-gallery';
 import styles from './styles.scss';
 
 export class Gallery extends React.Component {
@@ -12,29 +13,47 @@ export class Gallery extends React.Component {
 
     render() {
         const images = this.props.images;
-        const settings = {
-          dots: false,
-          infinite: true,
-          lazyLoad: true,
-          speed: 500,
-          centerMode: true,
-          draggable: false,
-          slidesToShow: 1,
-          slidesToScroll: 1
-        };
+        const galleryImages =  images.map((value, index)=>{
+          return {original: `/images/gallery/${value}`}
+          // return (<div key={index}><img src={`/images/gallery/${value}`}></img></div>)
+        })
+        console.log(galleryImages);
+        // const settings = {
+        //   dots: false,
+        //   infinite: true,
+        //   lazyLoad: true,
+        //   speed: 500,
+        //   centerMode: true,
+        //   draggable: false,
+        //   slidesToShow: 1,
+        //   slidesToScroll: 1
+        // };
         return (
             <div className='row'>
               <div className='columns medium-12'>
                 {
                   images && images.length > 0 ?
-                    <Slider {...settings}>
-                      {
-                        images.map((value, index)=>{
-                          console.log(value)
-                          return (<div key={index}><img src={`/images/gallery/${value}`}></img></div>)
-                        })
-                      }
-                    </Slider>
+                  <ImageGallery
+                      items={galleryImages}
+                      slideInterval={2000}
+                      showNav={false}
+                      showThumbnails={false}
+                      showPlayButton={false}
+                      showBullets={false}
+                      slideInterval={6000}
+                      autoPlay={true}
+                      lazyLoad={true}
+                    />
+
+
+                    // <Slider {...settings}>
+                    //   {
+                    //     images.map((value, index)=>{
+                    //       console.log(value)
+                    //       return (<div key={index}><img src={`/images/gallery/${value}`}></img></div>)
+                    //     })
+                    //   }
+                    // </Slider>
                   : <span>No Images Available</span>
                 }
               </div>
